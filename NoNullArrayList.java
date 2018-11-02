e4b07981e806081a6a89e8f7788b34e3d46dea59
Allen Wang <awang02@stuy.edu>
2018-11-02 14:46:27 -0400

    Worked on set method

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -3,5 +3,23 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   public NoNullArrayList(){
     super();
   }
+  public NoNullArrayList(int startingCapacity){
+    super(startingCapacity);
+  }
 
+  public Object set(int index, Object element){
+    if (element == null) {
+      throw new IllegalArgumentException();
+    }
+    else{
+      super.set(index,element);
+    }
+  }
+
+  public boolean add (Object o){
+
+  }
+  public void add (int index, Object element){
+
+  }
 }
