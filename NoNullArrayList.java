8174c2b6c09aadabc4af1f1a355834dd860c1032
Allen Wang <awang02@stuy.edu>
2018-11-05 03:07:13 -0500

    Completed set

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -7,13 +7,16 @@ public class NoNullArrayList<T> extends ArrayList<T> {
     super(startingCapacity);
   }
 
-  public Object set(int index, Object element){
+  public T set(int index, T element){
+    T object;
     if (element == null) {
-      throw new IllegalArgumentException();
+      throw new IllegalArgumentException("Null element invalid");
     }
     else{
+      object = this.get(index);
       super.set(index,element);
     }
+    return object;
   }
 
   public boolean add (Object o){
